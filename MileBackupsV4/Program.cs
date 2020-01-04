0b88283160cca00d98e8485457956dd80c730881
Leonardo Chuello <ljchuello@gmail.com>
2020-01-04 12:33:15 -0500

    Validando administrador

--- MileBackupsV4/Program.cs
+++ MileBackupsV4/Program.cs
@@ -1,9 +1,43 @@
-﻿namespace MileBackupsV4
+﻿using System;
+using System.Security.Principal;
+
+namespace MileBackupsV4
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                // Validamos si es administrador
+                if (!IsAdministrator())
+                {
+                    Console.WriteLine("Debe ejecutar el programa con privilegios de administrador, presione una tecla para salir");
+                    Console.Read();
+                    Environment.Exit(0);
+                }
+
+                Console.WriteLine("...");
+                Console.Read();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ah ocurrido un error; {ex.Message}");
+                Console.ReadLine();
+            }
+        }
+
+        /// <summary>
+        /// Valida si el usuario actual es administrador
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsAdministrator()
+        {
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
         }
     }
 }
